d47c5f0a83befc14f397b3c6905157f62c0be5d3
芮峰云 <rfyiamcool@163.com>
2015-09-19 00:57:01 +0800

    push 1.0

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+from distutils.core import setup, Command
+import os
+import os.path
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='geventcron',
+    version='1.0',
+    description='Gevent Crontab Scheduler',
+    long_description=open('README.md').read(),
+    keywords = ["gevent cron scheduler","fengyun"],
+    url='http://xiaorui.cc',
+    author='ruifengyun',
+    author_email='rfyiamcool@163.com',
+    install_requires=['gevent'],
+    packages=['geventcron'],
+    license = "MIT",
+    classifiers = [
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+            ]
+)
