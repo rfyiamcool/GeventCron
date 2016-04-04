b8ecaa016a12c2b1658e3ff86f858010c61f5748
rfyiamcool <rfyiamcool@163.com>
2016-04-04 17:54:00 +0800

    add crontab test.py

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
 #coding:utf-8
-import geventcron 
+import geventcron
 from datetime import datetime
 import time
 import os
@@ -19,15 +19,12 @@ def async(func):
 @async
 def func_1():
     print 'Call func_1'
-    time.sleep(2)
 
 def func_2():
     print 'Call func_2'
-    time.sleep(2)
 
 def func_3():
     print 'Call func_3'
-    time.sleep(2)
 
 #尽量别用堵塞的模块,可以用grequests
 def block():
@@ -35,7 +32,7 @@ def block():
 
 if __name__ == "__main__":
     scheduler = geventcron.Scheduler(logger_name='task_scheduler')
-    scheduler.schedule('task_1', geventcron.every_second(4), func_1)
-    scheduler.schedule('task_2', geventcron.every_second(1), func_2)
-    scheduler.schedule('task_3', geventcron.every_second(1), func_3)
+    scheduler.schedule('task_1', geventcron.Interval("*/1 * * * *"), func_1)
+    scheduler.schedule('task_2', geventcron.Interval(2), func_2)
+    scheduler.schedule('task_3', geventcron.Interval(3), func_3)
     scheduler.run_forever(start_at='once')
